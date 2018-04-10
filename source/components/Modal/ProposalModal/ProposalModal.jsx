e6c385bfcd23a4eed741bc371e17617968b81ec0
Tony Chupak <tony.chupak@gmail.com>
2018-04-10 20:33:32 +0300

    Add ability to validate add and validate proposal

--- source/components/Modal/ProposalModal/ProposalModal.jsx
+++ source/components/Modal/ProposalModal/ProposalModal.jsx
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 import Button from '../../Button/Button';
 
@@ -12,53 +13,133 @@ class ProposalModal extends PureComponent {
     verificationCode: ''
   };
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      description: '',
+      alias: '',
+      signature: ''
+    }
+  }
+
+  handleChangeTitle = (event) => {
+    const value = event.target.value;
+
+    this.setState(() => ({
+      title: value
+    }))
+  };
+
+  handleChangeDescription = (event) => {
+    const value = event.target.value;
+
+    this.setState(() => ({
+      description: value
+    }))
+  };
+
+  handleChangeAlias = (event) => {
+    const value = event.target.value;
+
+    this.setState(() => ({
+      alias: value
+    }))
+  };
+
+  handleChangeSignature = (event) => {
+    const value = event.target.value;
+
+    this.setState(() => ({
+      signature: value
+    }))
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    event.nativeEvent.stopImmediatePropagation();
+
+    const data = {
+      alias: this.state.alias,
+      ver_string: this.props.verificationCode,
+      signature: this.state.signature
+    };
+
+    axios.post('https://boolberry.com/API/validate.php', data)
+      .then(() => {
+        this.submitSuccess();
+      })
+      .catch(() => {
+        alert('Something wrong. Try again.')
+      })
+  };
+
+  submitSuccess = () => {
+    const data = {
+      title: this.state.title,
+      description: this.state.description,
+      alias: this.state.alias,
+      ver_string: this.props.verificationCode,
+      signature: this.state.signature
+    };
+
+    axios.post('https://boolberry.com/API/create_proposal.php', data)
+      .then(() => {
+        alert('Success');
+      })
+      .catch((error) => console.error(error))
+  };
 
   render() {
     const { verificationCode } = this.props;
-
+    const { title, description, alias, signature } = this.state;
     return (
       <div className='modal-content proposal-modal'>
-        <dl className='modal-content__list-values column'>
-          <dt>Title</dt>
-          <dd>
-            <input type='text' className='modal-content__input'/>
-          </dd>
-        </dl>
+        <form onSubmit={this.handleSubmit}>
+          <dl className='modal-content__list-values column'>
+            <dt>Title</dt>
+            <dd>
+              <input type='text' value={title} onChange={this.handleChangeTitle} className='modal-content__input'/>
+            </dd>
+          </dl>
 
-        <dl className='modal-content__list-values column'>
-          <dt>Description</dt>
-          <dd>
-            <textarea rows='4' className='modal-content__text-area'/>
-          </dd>
-        </dl>
+          <dl className='modal-content__list-values column'>
+            <dt>Description</dt>
+            <dd>
+              <textarea rows='4' value={description} onChange={this.handleChangeDescription} className='modal-content__text-area'/>
+            </dd>
+          </dl>
 
-        <dl className='modal-content__list-values'>
-          <dt>Alias Validation</dt>
-          <dd>
-            <a className='modal-content__link' href='#'>What is it?</a>
-          </dd>
-        </dl>
+          <dl className='modal-content__list-values'>
+            <dt>Alias Validation</dt>
+            <dd>
+              <a className='modal-content__link' href='#'>What is it?</a>
+            </dd>
+          </dl>
 
-        <dl className='modal-content__list-values'>
-          <dt>Alias</dt>
-          <dd>
-            <input className='modal-content__input-small' type='text'/>
-          </dd>
-        </dl>
+          <dl className='modal-content__list-values'>
+            <dt>Alias</dt>
+            <dd>
+              <input value={alias} onChange={this.handleChangeAlias} className='modal-content__input-small' type='text'/>
+            </dd>
+          </dl>
 
-        <dl className='modal-content__list-values'>
-          <dt>Ver string</dt>
-          <dd>{verificationCode}</dd>
-        </dl>
+          <dl className='modal-content__list-values'>
+            <dt>Ver string</dt>
+            <dd>{verificationCode}</dd>
+          </dl>
 
-        <dl className='modal-content__list-values'>
-          <dt>Signature</dt>
-          <dd>
-            <input className='modal-content__input-small' type='text'/>
-          </dd>
-        </dl>
+          <dl className='modal-content__list-values'>
+            <dt>Signature</dt>
+            <dd>
+              <input value={signature} onChange={this.handleChangeSignature} className='modal-content__input-small' type='text'/>
+            </dd>
+          </dl>
 
-        <Button className='modal-content__submit'>Submit</Button>
+          <Button className='modal-content__submit'>Submit</Button>
+        </form>
       </div>
     )
   }
